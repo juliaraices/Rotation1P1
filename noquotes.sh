4b32b803679c7538a65477a5b74f7f1cec5a90b4
[juliaraices] <[julia.raices@gmail.com]>
2017-10-19 11:28:10 +0200

    first try, starting functions

---
+++ noquotes.sh
@@ -0,0 +1,28 @@
+#!bin/bash
+
+#script to remove quotation marks from files made with julia
+
+sed 's/"//g' yeast_allinteractions1.txt >yeast_allinteraction.txt
+sed 's/"//g' celegans_allinteractions1.txt >celegans_allinteraction.txt
+sed 's/"//g' mouse_allinteractions1.txt >mouse_allinteraction.txt
+sed 's/"//g' human_allinteractions1.txt >human_allinteraction.txt
+sed 's/"//g' dmel_allinteractions1.txt >dmel_allinteraction.txt
+rm *_allinteractions1.txt
+
+
+sed 's/"//g' yeast_queryinteractions1.txt >yeast_queryinteraction.txt
+sed 's/"//g' celegans_queryinteractions1.txt >celegans_queryinteraction.txt
+sed 's/"//g' dmel_queryinteractions1.txt >dmel_queryinteraction.txt
+sed 's/"//g' mouse_queryinteractions1.txt >mouse_queryinteraction.txt
+sed 's/"//g' human_queryinteractions1.txt >human_queryinteraction.txt
+rm *_queryinteractions1.txt
+
+sed 's/"//g' yeast_numbers1.txt >yeast_numbers.txt
+sed 's/"//g' celegans_numbers1.txt >celegans_numbers.txt
+sed 's/"//g' mouse_numbers1.txt >mouse_numbers.txt
+sed 's/"//g' human_numbers1.txt >human_numbers.txt
+sed 's/"//g' dmel_numbers1.txt >dmel_numbers.txt
+
+sed 's/"//g' allorthologs_numbers1.txt >allorthologs_numbers.txt
+
+rm *_numbers1.txt
--- spp.sh
+++ spp.sh
@@ -9,32 +9,32 @@
 echo "initiating human"
 date
 
-#grep ^"9606\." protein.links.full.v10.5.txt.gz | awk '($10 != 0) {print $1, $2, $10}' > human_experimental.txt
+grep ^"9606\." protein.links.full.v10.5.txt.gz | awk '($10 != 0) {print $1, $2, $10}' > human_experimental.txt
 zgrep ^"9606\." protein.aliases.v10.5.txt.gz > human_aliases.txt
 
 echo "finished human. initiating mouse"
 date
 
-#grep ^"10090\." protein.links.full.v10.5.txt.gz | awk '($10 != 0) {print $1, $2, $10}' > mouse_experimental.txt
+grep ^"10090\." protein.links.full.v10.5.txt.gz | awk '($10 != 0) {print $1, $2, $10}' > mouse_experimental.txt
 zgrep ^"10090\." protein.aliases.v10.5.txt.gz > mouse_aliases.txt
 
 echo "finished mouse. initiating fly"
 date
 
-#grep ^"7227\." protein.links.full.v10.5.txt.gz | awk '($10 != 0) {print $1, $2, $10}' > dmel_experimental.txt
+grep ^"7227\." protein.links.full.v10.5.txt.gz | awk '($10 != 0) {print $1, $2, $10}' > dmel_experimental.txt
 zgrep ^"7227\." protein.aliases.v10.5.txt.gz > dmel_aliases.txt
 
 echo "finished fly. initiating c. elegans"
 date
 
-#grep ^"6239\." protein.links.full.v10.5.txt.gz | awk '($10 != 0) {print $1, $2, $10}' > celegans_experimental.txt
+grep ^"6239\." protein.links.full.v10.5.txt.gz | awk '($10 != 0) {print $1, $2, $10}' > celegans_experimental.txt
 zgrep ^"6239\." protein.aliases.v10.5.txt.gz > celegans_aliases.txt
 
 echo "finished c. elegans. initiating yeast"
 date
 
-#zgrep ^"4932\." protein.links.full.v10.5.txt.gz | awk '($10 != 0) {print $1, $2, $10}' > scerevisiae_experimental.txt
-zgrep ^"4932\." protein.aliases.v10.5.txt.gz > scerevisiae_aliases.txt
+zgrep ^"4932\." protein.links.full.v10.5.txt.gz | awk '($10 != 0) {print $1, $2, $10}' > yeast_experimental.txt
+zgrep ^"4932\." protein.aliases.v10.5.txt.gz > yeast_aliases.txt
 
 echo "finished yeast. bye bye"
 date
